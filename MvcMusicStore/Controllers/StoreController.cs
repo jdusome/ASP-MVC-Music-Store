7de450408e61f74f238b2edb39f6bdc3c7506f1b
jdusome <josh.dusome@live.ca>
2017-09-26 14:25:51 -0500

    end of lesson 3 - controllers

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Browse
+        public ActionResult Browse(string genre)
+        {
+            //add selected genre to the viewbag, so we can display in browse view
+            ViewBag.genre = genre;
+
+            return View();
+        }
+    }
+}
